746df472002161720db5e8d0321df2e922ad35fa
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-01-02 15:26:38 +0800

    Archloom

--- Old/src/main/java/com/texstudio/rubidium_toolkit/dynamic_lights/api/DynamicLightsInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.texstudio.rubidium_toolkit.dynamic_lights.api;
-
-public interface DynamicLightsInitializer
-{
-    /**
-     * Method called when LambDynamicLights is initialized to register custom dynamic light handlers and item light sources.
-     */
-    void onInitializeDynamicLights();
-}
