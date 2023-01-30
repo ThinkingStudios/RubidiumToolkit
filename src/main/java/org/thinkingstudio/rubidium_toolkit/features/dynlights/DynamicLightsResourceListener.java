74ebd94f74a5c66427a0aba1dcc052229f5a9005
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-01-30 23:01:23 +0800

    fix

--- src/main/java/org/thinkingstudio/rubidium_toolkit/features/dynlights/DynamicLightsResourceListener.java
+++ src/main/java/org/thinkingstudio/rubidium_toolkit/features/dynlights/DynamicLightsResourceListener.java
@@ -3,7 +3,6 @@ package org.thinkingstudio.rubidium_toolkit.features.dynlights;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.minecraft.server.packs.resources.ResourceManager;
-import org.jetbrains.annotations.NotNull;
 import org.thinkingstudio.rubidium_toolkit.features.dynlights.api.item.ItemLightSources;
 import net.minecraftforge.resource.IResourceType;
 import net.minecraftforge.resource.ISelectiveResourceReloadListener;
@@ -14,7 +13,7 @@ public class DynamicLightsResourceListener implements ISelectiveResourceReloadLi
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().serializeNulls().setLenient().create();
 
     @Override
-    public void onResourceManagerReload(@NotNull ResourceManager manager, @NotNull Predicate<IResourceType> resourcePredicate)
+    public void onResourceManagerReload(ResourceManager manager, Predicate<IResourceType> resourcePredicate)
     {
         ItemLightSources.load(manager);
     }
